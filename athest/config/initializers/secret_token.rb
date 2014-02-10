9e8aa60217f89adbd59490a5d5de5c3c760d66a1
ravi <ravi.teja@navyuginfo.com>
2014-02-10 18:05:47 +0530

    basic code added

---
+++ athest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Athest::Application.config.secret_token = '8f5d916ac65b3809d2e776841e0a74d445a1f04cd188bd69b4d7f45a2ab158823069bde5a8a50371d7f3d01bddf35aabdd5887dc465f5328a29c384eb05e2c62'
